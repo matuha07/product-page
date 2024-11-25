fdd9d54a42d2f4a9946084534ba8d4568ea36bbf
motya <motya228228@mail.ru>
2024-11-25 16:09:45 +0500

    merged feature-layout

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,58 @@
 
 <template>
 
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="text-center mt-4">Учёт товаров</h1>
+        <form action="">
+          <div class="mb-3">
+            <label for="name" class="form-label">Название</label>
+            <input type="text" class="form-control" id="name">
+          </div>
+          <div class="mb-3">
+            <label for="date" class="form-label">Дата приема товара</label>
+            <input type="date" class="form-control" id="date">
+          </div>
+          <div class="mb-3">
+            <label for="count" class="form-label">Количество</label>
+            <input type="number" class="form-control" id="count">
+          </div>
+          <div class="mb-3">
+            <label for="price" class="form-label">Цена</label>
+            <input type="number" class="form-control" id="price">
+          </div>
+          <div class="mb-2 text-center">
+            <button type="button" class="btn btn-outline-success">Добавить</button>
+          </div>
+        </form>
+      </div>
+    </div>
+
+
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Samsung</h5>
+        <p class="card-text">25.11.2024</p>
+        <p class="card-text">123412341235$</p>
+        <p class="card-text">x0</p>
+      </div>
+      <div class="card-footer">
+        <button class="btn btn-outline-danger">Удалить</button>
+      </div>
+    </div>
+  </div>
+
+</div>
+<div class="row my-4">
+    <div class="col">
+      <h3 class="text-end">Общая сумма: 0$</h3>
+    </div>
+  </div>
+  </div>
 </template>
 
 <style></style>
