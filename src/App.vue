3078257e5371ca69e3ae5384c6ad96df3381cc1f
motya <motya228228@mail.ru>
2024-11-25 16:07:00 +0500

    removed buttons

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,7 @@
 </script>
 
 <template>
-<button type="button" class="btn btn-primary">Primary</button>
-<button type="button" class="btn btn-secondary">Secondary</button>
-<button type="button" class="btn btn-success">Success</button>
-<button type="button" class="btn btn-danger">Danger</button>
-<button type="button" class="btn btn-warning">Warning</button>
-<button type="button" class="btn btn-info">Info</button>
-<button type="button" class="btn btn-light">Light</button>
-<button type="button" class="btn btn-dark">Dark</button>
+
 
 </template>
 
